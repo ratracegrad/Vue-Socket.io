73cd8b425d973148eacb617d22c1615f1273d80b
Jennifer Bland <ratracegrad@gmail.com>
2024-11-08 19:31:04 -0500

    start conversion to pinia

--- src/emitter.js
+++ src/emitter.js
@@ -1,14 +1,17 @@
 import Logger from './logger';
 
-export default class EventEmitter{
+export default class EventEmitter {
 
-    constructor(vuex = {}){
-        Logger.info(vuex ? `Vuex adapter enabled` : `Vuex adapter disabled`);
-        Logger.info(vuex.mutationPrefix ? `Vuex socket mutations enabled` : `Vuex socket mutations disabled`);
-        Logger.info(vuex ? `Vuex socket actions enabled` : `Vuex socket actions disabled`);
-        this.store = vuex.store;
-        this.actionPrefix = vuex.actionPrefix ? vuex.actionPrefix : 'SOCKET_';
-        this.mutationPrefix = vuex.mutationPrefix;
+    constructor(pinia = {}) {
+			console.log('start constructor', pinia)
+        Logger.info(pinia ? `Pinia adapter enabled` : `Pinia adapter disabled`);
+        Logger.info(pinia.mutationPrefix ? `Pinia socket mutations enabled` : `Pinia socket mutations disabled`);
+				console.log('mutationPrefix', pinia.mutationPrefix)
+        Logger.info(pinia ? `Pinia socket actions enabled` : `Pinia socket actions disabled`);
+        this.store = pinia.store;
+				console.log('store', this.store)
+        this.actionPrefix = pinia.actionPrefix ? pinia.actionPrefix : 'SOCKET_';
+        this.mutationPrefix = pinia.mutationPrefix;
         this.listeners = new Map();
     }
 
@@ -18,119 +21,53 @@ export default class EventEmitter{
      * @param callback
      * @param component
      */
-    addListener(event, callback, component){
+    addListener(event, callback, component) {
 
-        if(typeof callback === 'function'){
+        if (typeof callback === 'function') {
 
             if (!this.listeners.has(event)) this.listeners.set(event, []);
             this.listeners.get(event).push({ callback, component });
 
             Logger.info(`#${event} subscribe, component: ${component.$options.name}`);
-
-        } else {
-
-            throw new Error(`callback must be a function`);
-
         }
-
     }
 
     /**
-     * remove a listenler
-     * @param event
-     * @param component
-     */
-    removeListener(event, component){
-
-        if(this.listeners.has(event)){
-
-            const listeners = this.listeners.get(event).filter(listener => (
-                listener.component !== component
-            ));
-
-            if (listeners.length > 0) {
-                this.listeners.set(event, listeners);
-            } else {
-                this.listeners.delete(event);
-            }
-
-            Logger.info(`#${event} unsubscribe, component: ${component.$options.name}`);
-
-        }
-
-    }
-
-    /**
-     * broadcast incoming event to components
+     * emit event to all registered listeners
      * @param event
      * @param args
      */
-    emit(event, args){
-
-        if(this.listeners.has(event)){
-
-            Logger.info(`Broadcasting: #${event}, Data:`, args);
-
-            this.listeners.get(event).forEach((listener) => {
-                listener.callback.call(listener.component, args);
+    emit(event, ...args) {
+        if (this.listeners.has(event)) {
+            this.listeners.get(event).forEach(listener => {
+                listener.callback.apply(listener.component, args);
             });
-
         }
 
-        if(event !== 'ping' && event !== 'pong') {
-            this.dispatchStore(event, args);
-        }
+        if (this.store) {
+            if (this.mutationPrefix) {
+                const mutation = `${this.mutationPrefix}${event}`;
+                if (this.store.hasMutation(mutation)) {
+                    this.store.commit(mutation, ...args);
+                }
+            }
 
+            const action = `${this.actionPrefix}${event}`;
+            if (this.store.hasAction(action)) {
+                this.store.dispatch(action, ...args);
+            }
+        }
     }
-
 
     /**
-     * dispatching vuex actions
+     * remove event listener
      * @param event
-     * @param args
+     * @param callback
      */
-    dispatchStore(event, args){
-
-        if(this.store && this.store._actions){
-
-            let prefixed_event = this.actionPrefix + event;
-
-            for (let key in this.store._actions) {
-
-                let action = key.split('/').pop();
-
-                if(action === prefixed_event) {
-
-                    Logger.info(`Dispatching Action: ${key}, Data:`, args);
-
-                    this.store.dispatch(key, args);
-
-                }
-
-            }
-
-            if(this.mutationPrefix) {
-
-                let prefixed_event = this.mutationPrefix + event;
-
-                for (let key in this.store._mutations) {
-
-                    let mutation = key.split('/').pop();
-
-                    if(mutation === prefixed_event) {
-
-                        Logger.info(`Commiting Mutation: ${key}, Data:`, args);
-
-                        this.store.commit(key, args);
-
-                    }
-
-                }
-
-            }
-
+    removeListener(event, callback) {
+        if (this.listeners.has(event)) {
+            const listeners = this.listeners.get(event).filter(listener => listener.callback !== callback);
+            this.listeners.set(event, listeners);
         }
-
     }
-
 }
